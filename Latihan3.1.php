3444e5f5be1ccdc32c73e5dc9369d6987c1871bc
dzrymka <133848017+dzrymka@users.noreply.github.com>
2023-09-02 09:24:25 +0700

    Contoh pada bagian materi

---
+++ Latihan3.1.php
@@ -0,0 +1,36 @@
+<?php
+class Kendaraan
+{
+    var $jumlahRoda = 4;
+    var $warna;
+    var $bahanBakar = "Premium";
+    var $harga = 100000000;
+    var $merek;
+    var $tahunPembuatan = 2004;
+
+    function statusHarga()
+    {
+        if ($this->harga > 50000000) {
+            $status = "Harga Kendaraan Mahal";
+        } else {
+            $status = "Harga Kendaraan Murah";
+        }
+        return $status;
+    }
+
+    function statusSubsidi()
+    {
+        if ($this->tahunPembuatan < 2005 && $this->bahanBakar == "Premium") {
+            $status = "DAPAT SUBSIDI";
+        } else {
+            $status = "TIDAK DAPAT SUBSIDI";
+        }
+        return $status;
+    }
+}
+
+// Instansiasi kelas
+$ObjekKendaraan = new Kendaraan(); // Pembuatan objek dari kelas
+echo "Jumlah Roda : " . $ObjekKendaraan->jumlahRoda . "<br />"; // Proses instansiasi/pemanggilan variable
+echo "Status Harga : " . $ObjekKendaraan->statusHarga() . "<br />"; // Proses instansiasi/pemanggilan function dari kelas
+echo "Status Subsidi : " . $ObjekKendaraan->statusSubsidi();
